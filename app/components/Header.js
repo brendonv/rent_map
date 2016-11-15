248f4a2344b8608048907e23fe5e67732a557342
Brendon <brendon.verissimo@gmail.com>
2016-11-15 10:53:00 -0800

    Basic dom structure

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,9 +1,21 @@
 import React, { Component, PropTypes } from 'react'
+require('../style/header.css')
 
 export default class Header extends Component {
   render() {
     const { city, region} = this.props
-    return <div> {city} - {region} </div>
+    return (
+        <div id="header">
+            <div id="locale" className="">
+                <div className="city">
+                    <h1>{city}</h1>
+                </div>
+                <div className="region">
+                    <h3>{region === 'ALL' ? `${region} NEIGHBORHOODS` : region}</h3>
+                </div>
+            </div>
+        </div>
+        )
   }
 }
 
